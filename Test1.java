b916f689de632bfd3fb5e078e293775189b693ab
Antusadg <149178173+Antusadg@users.noreply.github.com>
2024-08-02 15:17:03 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1{
+
+
+
+// this method is to add numbers
+public int addNumbers(int x,int y){
+	return x+y;
+
+public int subtract(int a,int b){
+	return a-b;
+}
+
+}
---
+++ TestMain.java
@@ -0,0 +1,9 @@
+public class TestMain(){
+
+public static vois main(String args[]){
+
+	Test1 t=new Test1();
+	System.out.println(t.addNumbers(3,19));
+
+}
+}
